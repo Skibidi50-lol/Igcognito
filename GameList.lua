146d91dd1d9e5b38b047955fdd0edb095d6c5db1
Skibidi50-lol <realhatroi@gmail.com>
2026-04-02 17:49:13 +0700

    Create GameList.lua

---
+++ GameList.lua
@@ -0,0 +1,10 @@
+local Games = {
+  [77393318863643] = "https://raw.githubusercontent.com/Skibidi50-lol/Igcognito/refs/heads/main/Scripts/AuraAscension.lua", -- Aura Ascension
+  [79311273910901] = "https://raw.githubusercontent.com/Skibidi50-lol/Igcognito/refs/heads/main/Scripts/BladeSpin.lua", --Blade Spin
+  [84514721527976] = "https://raw.githubusercontent.com/Skibidi50-lol/Igcognito/refs/heads/main/Scripts/DigDeeperForBrainrots.lua", --Dig Deeper For Brainrots
+  [140317247681516] = "https://raw.githubusercontent.com/Skibidi50-lol/Igcognito/refs/heads/main/Scripts/JumpClicker.lua", --Jump Clicker
+  [80861715191104] = "https://raw.githubusercontent.com/Skibidi50-lol/Igcognito/refs/heads/main/Scripts/PullLuckyBlocks.lua", --Pull Lucky Blocks
+  [114640202062357] = "https://raw.githubusercontent.com/Skibidi50-lol/Igcognito/refs/heads/main/Scripts/Scripts/SwingObbyForBrainrots.lua", --Swing Obby For Brainrots
+}
+
+return Games
